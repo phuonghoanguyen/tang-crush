7074f4034a9bfade3651fd6280ef611f3b842a14
phuonghoanguyen <89855111+phuonghoanguyen@users.noreply.github.com>
2021-08-31 21:33:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'thế giới của anh!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    title: 'thùy chi à',
+    desc: 'ấn zô đây',
+    btnYes: 'đúng vậy chuẩn',
+    btnNo: 'Không iu quý lắm á',
+    question:'homie m ghét hngoc lắm đúng k',
+    btnReply: '...',
+    reply: 'chửi nó 1 câu coi',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
